29e1e04375c7f5b283dd5ca65593f4a62b9d773b
michaschwab <me@michaschwab.de>
2020-02-19 18:57:10 -0500

    Load extension JS synchronously to avoid being added after other code has loaded

--- extension/inject.js
+++ extension/inject.js
@@ -1,13 +1,21 @@
 (function() {
 	const inject = () => {
 		// Code from https://unpkg.com/peerjs@1.0.4/dist/peerjs.min.js
-		// and from https://michaschwab.github.io/DESCVis/descvis-bundle-no-ui.js
+		// and from https://michaschwab.github.io/DESCVis/descvis-bundle.js
 		const sources = ['peerjs.1.0.4.min.js', 'descvis-bundle.js'];
 
 		for(const source of sources) {
-			const script = document.createElement('script');
-			script.src = chrome.extension.getURL(source);
-			(document.head||document.documentElement).prepend(script);
+			const url = chrome.runtime.getURL(source);
+
+			const request = new XMLHttpRequest();
+			request.open('GET', url, false); // Synchronous
+			request.send(null);
+
+			if (request.status === 200) {
+				const script = document.createElement('script');
+				script.textContent = request.responseText;
+				(document.head||document.documentElement).prepend(script);
+			}
 		}
 	};
 
