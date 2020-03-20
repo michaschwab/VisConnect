dfafbcd9454a5ef89ce8ee0e5c9512a9fe8709c6
michaschwab <me@michailschwab.com>
2020-03-20 16:07:12 -0400

    add some icons for people and rocks

--- examples/firefighters/main.js
+++ examples/firefighters/main.js
@@ -10,36 +10,49 @@ svg.append('rect')
     .attr('fill', '#ccc');
 var net = svg.append('line').attr('stroke', '#000000');
 var collaboratorIds = new Set();
+var started = false;
 svg.on('mousemove', function () {
     var index = getCollaboratorIndex(d3.event.collaboratorId);
     net.attr('x' + String(index), d3.event.x);
     net.attr('y' + String(index), d3.event.y);
+    if (!started) {
+        started = true;
+        raf();
+    }
 });
 var addRandomFallingObject = function () {
     fallingObjects.push({
+        id: String(random()),
         type: random() > 0.5 ? 'rock' : 'person',
         position: { x: random() * width, y: -100 },
-        speed: random() * 5
+        speed: 0.1 + random() * 0.2
     });
 };
+var lastFrame = Date.now();
+var rockPath = 'm 20,135 20,-10 60,10 30,-10 10,20 -20,25 -60,10 z';
+var personPath = 'm 42,165 c 0,0 -10,6 -12,-3 -1,-8 14,-6 20,-4 5,1 8,-2 24,-3 15,-0 66,-6 66,-6 0,0 12,0 1,6 -13,2 -28,3 -28,3 l 27,1 c 0,0 18,3 0,5 -17,-0 -44,-0 -44,-0 l -21,1 -19,0 c 0,0 -15,20 -19,22 -4,2 -8,0 -2,-6 6,-7 10,-11 10,-11 l -19,7 c 0,0 -11,1 -3,-4 10,-2 20,-8 20,-8 z';
 var raf = function () {
     if (random() > 0.9) {
         addRandomFallingObject();
     }
-    fallingObjects.forEach(function (falling) { return falling.position.y += falling.speed; }); //TODO: Set by time, not by RAF.
+    fallingObjects.forEach(function (falling) { return falling.position.y += falling.speed * (Date.now() - lastFrame); });
     fallingObjects = fallingObjects.filter(function (falling) { return falling.position.y < height; });
-    var falling = svg.selectAll('.falling').data(fallingObjects);
+    var falling = svg.selectAll('.falling').data(fallingObjects, function (d) { return d.id; });
+    falling.exit().remove();
     var fallingEnter = falling.enter()
         .append('g')
         .attr('class', 'falling');
-    fallingEnter.append('circle')
-        .attr('r', 5)
+    fallingEnter.append('path')
+        .attr('d', function (d) { return d.type === 'rock' ? rockPath : personPath; })
         .attr('fill', function (d) { return d.type === 'rock' ? '#666' : '#0c0'; });
-    falling.attr('transform', function (d) { return "translate(" + d.position.x + ", " + d.position.y + ")"; });
+    fallingEnter.merge(falling).attr('transform', function (d) { return "translate(" + d.position.x + ", " + d.position.y + ")scale(0.5)"; });
+    lastFrame = Date.now();
     requestAnimationFrame(raf);
 };
-raf();
 var getCollaboratorIndex = function (id) {
+    if (!id) {
+        return;
+    }
     if (!collaboratorIds.has(id)) {
         collaboratorIds.add(id);
     }
--- examples/firefighters/main.ts
+++ examples/firefighters/main.ts
@@ -4,6 +4,7 @@ interface FallingObject {
     type: 'rock'|'person';
     position: {x: number, y: number};
     speed: number;
+    id: string;
 }
 
 const svg = d3.select('svg');
@@ -21,49 +22,64 @@ svg.append('rect')
 
 const net = svg.append('line').attr('stroke', '#000000');
 const collaboratorIds = new Set<string>();
+let started = false;
 
 svg.on('mousemove', () => {
     const index = getCollaboratorIndex(d3.event.collaboratorId);
     net.attr('x' + String(index), d3.event.x);
     net.attr('y' + String(index), d3.event.y);
+
+    if(!started) {
+        started = true;
+        raf();
+    }
 });
 
 
 const addRandomFallingObject = () => {
     fallingObjects.push({
+        id: String(random()),
         type: random() > 0.5 ? 'rock' : 'person',
         position: {x: random() * width, y: -100},
-        speed: random() * 5
+        speed: 0.1 + random() * 0.2
     });
 };
+
+let lastFrame = Date.now();
+const rockPath = 'm 20,135 20,-10 60,10 30,-10 10,20 -20,25 -60,10 z';
+const personPath = 'm 42,165 c 0,0 -10,6 -12,-3 -1,-8 14,-6 20,-4 5,1 8,-2 24,-3 15,-0 66,-6 66,-6 0,0 12,0 1,6 -13,2 -28,3 -28,3 l 27,1 c 0,0 18,3 0,5 -17,-0 -44,-0 -44,-0 l -21,1 -19,0 c 0,0 -15,20 -19,22 -4,2 -8,0 -2,-6 6,-7 10,-11 10,-11 l -19,7 c 0,0 -11,1 -3,-4 10,-2 20,-8 20,-8 z';
 
 const raf = () => {
     if(random() > 0.9) {
         addRandomFallingObject();
     }
 
-    fallingObjects.forEach(falling => falling.position.y += falling.speed); //TODO: Set by time, not by RAF.
+    fallingObjects.forEach(falling => falling.position.y += falling.speed * (Date.now() - lastFrame));
     fallingObjects = fallingObjects.filter(falling => falling.position.y < height);
 
-    const falling = svg.selectAll('.falling').data(fallingObjects);
+    const falling = svg.selectAll('.falling').data(fallingObjects, d => d.id);
+    falling.exit().remove();
     const fallingEnter = falling.enter()
         .append('g')
         .attr('class', 'falling');
 
-    fallingEnter.append('circle')
-        .attr('r', 5)
+    fallingEnter.append('path')
+        .attr('d', (d) => d.type === 'rock' ? rockPath : personPath)
         .attr('fill', (d) => d.type === 'rock' ? '#666' : '#0c0');
 
-    falling.attr('transform', (d) => `translate(${d.position.x}, ${d.position.y})`);
+    fallingEnter.merge(falling).attr('transform', (d) => `translate(${d.position.x}, ${d.position.y})scale(0.5)`);
+    lastFrame = Date.now();
 
     requestAnimationFrame(raf);
 };
 
-raf();
 
 
 
 const getCollaboratorIndex = (id: string) => {
+    if(!id) {
+        return;
+    }
     if(!collaboratorIds.has(id)) {
         collaboratorIds.add(id);
     }
