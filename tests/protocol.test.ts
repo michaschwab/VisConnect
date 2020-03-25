ba6ffefe484fc27035eca239d2cf869a64c6c510
michaschwab <me@michailschwab.com>
2020-03-25 19:02:55 -0400

    more testing setup

--- tests/protocol.test.ts
+++ tests/protocol.test.ts
@@ -1,8 +1,16 @@
 import {VcProtocol} from "../src/protocol";
 import {VcCommunication, VcCommunicationI} from "../src/communication";
+import {VcLeaderProtocol} from "../src/leader-protocol";
 
 function test() {
-    const leaderId = '';
+    const leaderId = 'leader';
+    const leader = getMockClient(leaderId, leaderId);
+    const client = getMockClient(leaderId, 'client');
+
+
+}
+
+function getMockClient(id: string, leaderId: string) {
     const execute = () => {
 
     };
@@ -10,9 +18,11 @@ function test() {
 
     };
     const communication: VcCommunicationI = {
-        getId: () => 'test',
+        getId: () => id,
         broadcastEvent: () => {},
         requestLock: (selector) => true
     };
-    const protocol = new VcProtocol(leaderId, execute, cancel, [], communication);
+    const isLeader = id === leaderId;
+    const Protocol = isLeader ? VcLeaderProtocol : VcProtocol;
+    const protocol = new Protocol(leaderId, execute, cancel, [], communication);
 }
